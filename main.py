baf97452d2b7a5d2c06b56bd4b4418ed29b305ff
Parthsarthi Joshi <parthucmas@gmail.com>
2024-03-27 13:58:45 +0530

    exception,logging & utils

---
+++ main.py
@@ -0,0 +1,3 @@
+from mlProject import logger # Though logger is inside mlProject folder, we need not to write src.mlProject because we wrote the logging code inside the __init__.py
+
+logger.info("Welcome to fist log")
