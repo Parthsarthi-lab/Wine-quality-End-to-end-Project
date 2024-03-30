b1a81778df0fdddec6cfcbca812d462fe3a27f37
Parthsarthi Joshi <parthucmas@gmail.com>
2024-03-30 14:10:02 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from mlProject import logger # Though logger is inside mlProject folder, we need not to write src.mlProject because we wrote the logging code inside the __init__.py
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainigPipeline
 
-logger.info("Welcome to fist log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionTrainigPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
