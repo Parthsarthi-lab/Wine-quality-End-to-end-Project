744655bc6ed003c342e9a327d22e60810a3b971f
Parthsarthi Joshi <parthucmas@gmail.com>
2024-03-31 12:35:50 +0530

    Data Validation Done

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from mlProject import logger # Though logger is inside mlProject folder, we need not to write src.mlProject because we wrote the logging code inside the __init__.py
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainigPipeline
-
+from mlProject.pipeline.stage_02_data_validation import DataValidationPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<")
-    obj = DataIngestionTrainigPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainigPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
---
+++ src/mlProject/pipeline/stage_02_data_validation.py
@@ -0,0 +1,26 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_validation import DataValidation
+from mlProject import logger
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        """Initializes the pipeline"""
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+if __name__ =="__main__":
+    try:
+          logger.info(f">>>>> stage {STAGE_NAME} started <<<<")
+          obj = DataValidationPipeline()
+          obj.main()
+          logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+    except Exception as e:
+          logger.exception(e)
+          raise e
